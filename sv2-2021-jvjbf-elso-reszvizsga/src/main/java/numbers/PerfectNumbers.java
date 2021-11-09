8e44fd02f7b6bf52ffbf0e2160903ca186fbb4e0
Morsiani Renato <m.renato@freemail.hu>
2021-11-09 15:36:14 +0100

    PerfectNumbers task done

---
+++ sv2-2021-jvjbf-elso-reszvizsga/src/main/java/numbers/PerfectNumbers.java
@@ -0,0 +1,15 @@
+package numbers;
+
+public class PerfectNumbers {
+
+    public boolean isPerfectNumber(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum = sum + i;
+            }
+        }
+        return sum == number;
+    }
+}
+
